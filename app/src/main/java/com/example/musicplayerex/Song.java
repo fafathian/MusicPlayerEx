bb840e3baac5db5cac17d008d00f1a6b0d1e816b
fafathian <zulfa.fathian.ft18@mail.umy.ac.id>
2020-05-17 16:32:39 +0700

    Insya Allah done

---
+++ app/src/main/java/com/example/musicplayerex/Song.java
@@ -0,0 +1,20 @@
+package com.example.musicplayerex;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
+
